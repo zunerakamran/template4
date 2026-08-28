63066d4e21a054b535a500a61118226b90336d1e
zunerakamran <zunairakamran000@gmail.com>
2026-08-28 14:20:57 +0500

    hero section images fixed

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
     API_URL: "./api.php",
-    ADVISOR_ID: 6
+    ADVISOR_ID: 6,
+    // Laravel public origin for advisor-uploaded images (not the cPanel api.php).
+    UPLOADS_ORIGIN: 'https://devznr.epatronus.net/compliance/api',
 }
 
 export default CONFIG
