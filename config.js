5b84f89f5bbc5cd2c04640185cc5588c0ffc301e
zunerakamran <zunairakamran000@gmail.com>
2026-08-28 14:30:05 +0500

    Local header images

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     API_URL: "./api.php",
     ADVISOR_ID: 6,
     // Laravel public origin for advisor-uploaded images (not the cPanel api.php).
-    UPLOADS_ORIGIN: 'https://devznr.epatronus.net/compliance/api',
+    UPLOADS_ORIGIN: 'https://devznr.epatronus.net/compliance/api/api',
 }
 
 export default CONFIG
